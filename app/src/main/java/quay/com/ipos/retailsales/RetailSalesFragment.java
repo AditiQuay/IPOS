f8ea193352c6ea947e0980c89e63e997f14c61cf
Aditi Bhuranda <aditi.bhuranda@crystal.com>
2018-04-16 16:59:38 +0530

    first

---
+++ app/src/main/java/quay/com/ipos/retailsales/RetailSalesFragment.java
@@ -0,0 +1,12 @@
+package quay.com.ipos.retailsales;
+
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by aditi.bhuranda on 16-04-2018.
+ */
+
+public class RetailSalesFragment extends Fragment {
+
+}
