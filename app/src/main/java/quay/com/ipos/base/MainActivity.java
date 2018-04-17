9a05be3a93b219fa26fa827811b79aaad13621da
aditi.bhuranda <aditi.bhuranda@quayintech.com>
2018-04-17 12:15:17 +0530

    retail

--- app/src/main/java/quay/com/ipos/base/MainActivity.java
+++ app/src/main/java/quay/com/ipos/base/MainActivity.java
@@ -1,19 +1,11 @@
 package quay.com.ipos.base;
 
-import android.app.SearchManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.widget.SearchView;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import quay.com.ipos.R;
@@ -21,24 +13,18 @@ import quay.com.ipos.retailsales.RetailSalesFragment;
 
 public class MainActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private int mContainerId;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.appBar);
         setSupportActionBar(toolbar);
+        mContainerId = R.id.fragment_container;
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -46,11 +32,12 @@ public class MainActivity extends BaseActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        addFragment(new RetailSalesFragment(),mContainerId);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -59,31 +46,8 @@ public class MainActivity extends BaseActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        // Retrieve the SearchView and plug it into SearchManager
-        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
-        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        return true;
-    }
 
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -93,16 +57,10 @@ public class MainActivity extends BaseActivity
 
         if (id == R.id.nav_retail_sales) {
             // Handle the camera action
-            addFragment(new RetailSalesFragment(),id);
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
+            addFragment(new RetailSalesFragment(),mContainerId);
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
--- app/src/main/java/quay/com/ipos/retailsales/RetailSalesFragment.java
+++ app/src/main/java/quay/com/ipos/retailsales/RetailSalesFragment.java
@@ -1,22 +1,46 @@
 package quay.com.ipos.retailsales;
 
 
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import quay.com.ipos.R;
+import quay.com.ipos.ui.FontManager;
 
 /**
  * Created by aditi.bhuranda on 16-04-2018.
  */
 
 public class RetailSalesFragment extends Fragment {
+    private TextView tvUserAdd,tvPin,tvRedeem;
+    private ImageView imvQRCode,imvGlobe;
+    private SwitchCompat switchButton;
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.retail_dashboard, container, false);
+        initializeComponent(rootView);
+        return rootView;
+    }
+
+    private void initializeComponent(View rootView) {
+        tvUserAdd = rootView.findViewById(R.id.tvUserAdd);
+        tvPin = rootView.findViewById(R.id.tvPin);
+        tvRedeem = rootView.findViewById(R.id.tvRedeem);
+        imvGlobe = rootView.findViewById(R.id.imvGlobe);
+        imvQRCode = rootView.findViewById(R.id.imvQRCode);
+
+        Typeface iconFont = FontManager.getTypeface(getActivity(), FontManager.FONTAWESOME);
+        FontManager.markAsIconContainer(tvUserAdd, iconFont);
+        FontManager.markAsIconContainer(tvPin, iconFont);
+        FontManager.markAsIconContainer(tvRedeem, iconFont);
+
     }
 }
