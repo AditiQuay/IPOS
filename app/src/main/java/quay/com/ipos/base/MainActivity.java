76568675f52c1ab5f2a093f55e73ee0fc34d71cd
aditi.bhuranda <aditi.bhuranda@quayintech.com>
2018-04-17 18:24:45 +0530

    retail

--- app/src/main/java/quay/com/ipos/base/MainActivity.java
+++ app/src/main/java/quay/com/ipos/base/MainActivity.java
@@ -1,12 +1,18 @@
 package quay.com.ipos.base;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import quay.com.ipos.R;
 import quay.com.ipos.retailsales.RetailSalesFragment;
@@ -14,6 +20,8 @@ import quay.com.ipos.retailsales.RetailSalesFragment;
 public class MainActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     private int mContainerId;
+    private static final int CAMERA_PERMISSION = 1;
+    private Class<?> mClss;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -48,6 +56,33 @@ public class MainActivity extends BaseActivity
 
 
 
+    public void launchActivity(Class<?> clss) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            mClss = clss;
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION);
+        } else {
+            Intent intent = new Intent(this, clss);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,  String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case CAMERA_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if(mClss != null) {
+                        Intent intent = new Intent(this, mClss);
+                        startActivity(intent);
+                    }
+                } else {
+                    Toast.makeText(this, "Please grant camera permission to use the QR Scanner", Toast.LENGTH_SHORT).show();
+                }
+                return;
+        }
+    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
--- app/src/main/java/quay/com/ipos/retailsales/RetailSalesFragment.java
+++ app/src/main/java/quay/com/ipos/retailsales/RetailSalesFragment.java
@@ -10,37 +10,68 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import quay.com.ipos.R;
+import quay.com.ipos.base.MainActivity;
 import quay.com.ipos.ui.FontManager;
 
 /**
  * Created by aditi.bhuranda on 16-04-2018.
  */
 
-public class RetailSalesFragment extends Fragment {
+public class RetailSalesFragment extends Fragment implements View.OnClickListener {
     private TextView tvUserAdd,tvPin,tvRedeem;
-    private ImageView imvQRCode,imvGlobe;
-    private SwitchCompat switchButton;
+    private ImageView imvDicount,imvGlobe,imvQRCode;
+    private ToggleButton chkBarCode;
 
     @Override
     public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.show_product_list_item, container, false);
+        View rootView = inflater.inflate(R.layout.retail_dashboard, container, false);
         initializeComponent(rootView);
         return rootView;
     }
 
     private void initializeComponent(View rootView) {
-//        tvUserAdd = rootView.findViewById(R.id.tvUserAdd);
-//        tvPin = rootView.findViewById(R.id.tvPin);
-//        tvRedeem = rootView.findViewById(R.id.tvRedeem);
-//        imvGlobe = rootView.findViewById(R.id.imvGlobe);
-//        imvQRCode = rootView.findViewById(R.id.imvQRCode);
-//
-//        Typeface iconFont = FontManager.getTypeface(getActivity(), FontManager.FONTAWESOME);
-//        FontManager.markAsIconContainer(tvUserAdd, iconFont);
-//        FontManager.markAsIconContainer(tvPin, iconFont);
-//        FontManager.markAsIconContainer(tvRedeem, iconFont);
+        tvUserAdd = rootView.findViewById(R.id.tvUserAdd);
+        tvPin = rootView.findViewById(R.id.tvPin);
+        tvRedeem = rootView.findViewById(R.id.tvRedeem);
+        imvGlobe = rootView.findViewById(R.id.imvGlobe);
+        imvDicount = rootView.findViewById(R.id.imvDicount);
+        imvQRCode = rootView.findViewById(R.id.imvQRCode);
+        chkBarCode = rootView.findViewById(R.id.chkBarCode);
 
+        Typeface iconFont = FontManager.getTypeface(getActivity(), FontManager.FONTAWESOME);
+        FontManager.markAsIconContainer(tvUserAdd, iconFont);
+        FontManager.markAsIconContainer(tvPin, iconFont);
+        FontManager.markAsIconContainer(tvRedeem, iconFont);
+        imvQRCode.setOnClickListener(this);
+        chkBarCode.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                if (chkBarCode.isChecked()) {
+                    imvQRCode.setVisibility(View.VISIBLE);
+                    Toast.makeText(getActivity(), "ON", Toast.LENGTH_SHORT).show();
+                } else {
+                    imvQRCode.setVisibility(View.GONE);
+                    Toast.makeText(getActivity(), "OFF", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        switch (id){
+            case R.id.imvQRCode:
+                ((MainActivity) getActivity()).launchActivity(FullScannerActivity.class);
+                break;
+        }
     }
 }
