d083ab303321ac51851435af8796846d5a22fe55
ankush.bansal <ankush.bansal@quayintech.com>
2018-07-11 10:27:03 +0530

    fefd

---
+++ app/src/main/java/quay/com/ipos/inventoryTrasfer/modal/TransferOutListModal.java
@@ -0,0 +1,72 @@
+package quay.com.ipos.inventoryTrasfer.modal;
+
+/**
+ * Created by ankush.bansal on 09-07-2018.
+ */
+
+public class TransferOutListModal {
+
+    private String senderName;
+    private String senderAddress;
+    private String receiverName;
+    private String receiverAddress;
+    private double amount;
+    private String id;
+    private String date;
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getSenderAddress() {
+        return senderAddress;
+    }
+
+    public void setSenderAddress(String senderAddress) {
+        this.senderAddress = senderAddress;
+    }
+
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
+    }
+
+    public String getReceiverAddress() {
+        return receiverAddress;
+    }
+
+    public void setReceiverAddress(String receiverAddress) {
+        this.receiverAddress = receiverAddress;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
